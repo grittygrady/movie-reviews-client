5cbaa194ef69412bb101793ac2fdecacbcded832
Christopher Grady <grittygrady@gmail.com>
2021-02-02 09:06:29 -0800

    minor progress

--- src/components/ReviewDetails.js
+++ src/components/ReviewDetails.js
@@ -6,20 +6,21 @@ import config from '../config';
 const ReviewDetails = (props) => {
   const { id } = useParams();
   // const { data: review, isLoading, error } = useFetch(`${config.API_ENDPOINT}/reviews/${id}`);
-  const { data: review, isLoading, error } = props;
+  const { reviews } = props;
+  console.log(reviews)
 
   return ( 
     <div className="review-details">
-      {isLoading && <div>Loading...</div>}
-      {error && <div>{error}</div>}
-      {review && (
+      {/* {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>} */}
+      {reviews && reviews.map((review) => (
         <article>
           <h2>{review.title}</h2>
           <p>Rating: {review.rating}</p>
           <p>Written by {review.author}</p>
           <div>{review.body}</div>
         </article>
-      )}
+      ))}
     </div>
   );
 }
