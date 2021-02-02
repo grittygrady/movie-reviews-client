0e313fc737775c29f19772ebaec3b433b3cf3e79
Christopher Grady <grittygrady@gmail.com>
2021-02-02 13:22:08 -0800

    revert

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddReview from './components/AddReview';
+import ReviewDetails from './components/ReviewDetails';
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path = '/'>
+              <Home />
+            </Route>
+            <Route path = '/addreview'>
+              <AddReview />
+            </Route>
+            <Route path = '/reviews/:id'>
+              <ReviewDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-
-const inDev = process.env.NODE_ENV === 'development'
 export default {
-  API_ENDPOINT: 
-  inDev ? 'http://localhost:8000/api' : 
-  'https://thawing-sierra-58826.herokuapp.com/api'
+  API_ENDPOINT: 'http://localhost:8000'
 }
+// const inDev = process.env.NODE_ENV === 'development'
+// export default {
+//   API_ENDPOINT: inDev ? 'http://localhost:8000/api' : 'https://lit-mesa-39960.herokuapp.com/api'
+// }
