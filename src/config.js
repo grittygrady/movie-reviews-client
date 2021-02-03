5089c201b02f59376bf8762bdef070447046437c
Christopher Grady <grittygrady@gmail.com>
2021-02-03 12:18:18 -0800

    update config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 
-const inDev = process.env.NODE_ENV === 'development'
-export default {
-  API_ENDPOINT: 
-  inDev ? 'http://localhost:8000' : 
-  'https://thawing-sierra-58826.herokuapp.com'
-}
+// const inDev = process.env.NODE_ENV === 'development'
+// export default {
+//   API_ENDPOINT: 
+//   inDev ? 'http://localhost:8000' : 
+//   'https://thawing-sierra-58826.herokuapp.com'
+// }
+
+const config = { API_ENDPOINT: inDev ? 'http://localhost:8000' : 'https://thawing-sierra-58826.herokuapp.com' };
+
+export default config;
